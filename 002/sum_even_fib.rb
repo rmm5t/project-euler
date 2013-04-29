60e4b3902c7de39de3090b5dd3b02e78f1d3d7c9
Ryan McGeary <ryan@mcgeary.org>
2013-04-29 18:35:34 -0400

    Added project 2
    Tried playing with fibers

---
+++ 002/sum_even_fib.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+fibonaccis = Fiber.new do
+  a, b = 0, 1
+  Fiber.yield a
+  Fiber.yield b
+  loop do
+    c = a + b
+    a, b = b, c
+    Fiber.yield c
+  end
+end
+
+max = (ARGV[0] || 4_000_000).to_i
+sum = 0
+loop do
+  n = fibonaccis.resume
+  break if n > max
+  sum += n if n.even?
+end
+
+print "Sum of the even Fibonacci values that do not exceed #{max}: "
+puts sum
