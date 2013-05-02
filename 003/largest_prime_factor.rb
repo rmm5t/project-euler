48703eb5b3740560dd8f03c81594fb9f50749d14
Ryan McGeary <ryan@mcgeary.org>
2013-05-02 10:12:43 -0400

    Fixed problem 3 to avoid non prime divisors

--- 003/largest_prime_factor.rb
+++ 003/largest_prime_factor.rb
@@ -4,4 +4,4 @@ require "prime"
 
 number = (ARGV[0] || 600_851_475_143).to_i
 print "The largest prime factor of the #{number} is: "
-puts Prime.prime_division(number).flatten.max
+puts Prime.prime_division(number).transpose.first.max
