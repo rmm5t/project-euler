3bdba0a0324b9b8c4ed764a7ad4fa01de7c20d2d
Daniel Ackerman <daniel.joseph.ackerman@gmail.com>
2014-04-27 19:02:12 -0400

    Changed variable name from number to max in project 4

--- 004/largest_palindrome_product.rb
+++ 004/largest_palindrome_product.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-number = (ARGV[0] || 1000).to_i
-abort("Cannot input a negative number") unless number > 0
-print "The largest palindrome created by the product of two positive numbers under #{number} is: "
-puts (1...number).flat_map { |n|
-  (n...number).flat_map { |t|
+max = (ARGV[0] || 1000).to_i
+abort("Cannot input a negative number") unless max > 0
+print "The largest palindrome created by the product of two positive numbers under #{max} is: "
+puts (1...max).flat_map { |n|
+  (n...max).flat_map { |t|
     product = (n*t).to_s;
     product.to_i if (product.reverse == product)
   }
