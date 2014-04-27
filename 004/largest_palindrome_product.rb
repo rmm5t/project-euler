52ab01a870cc486cd48db535619bf97eef707663
Daniel Ackerman <daniel.joseph.ackerman@gmail.com>
2014-04-27 18:50:30 -0400

    Modified project 4 to follow ruby conventions

--- 004/largest_palindrome_product.rb
+++ 004/largest_palindrome_product.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 
 number = (ARGV[0] || 1000).to_i
-res = 1
-(1...number).each_entry { |n|
-  ((n)...number).each_entry { |t|
-    product = (t*n).to_s
-    (res = product.to_i) if (product.reverse == product && product.to_i > res)
-  }
-}
 print "The largest palindrome created by the product of two positive numbers under #{number} is: "
-puts res
+puts (1...number).flat_map { |n|
+  (n...number).flat_map { |t|
+    product = (n*t).to_s;
+    product.to_i if (product.reverse == product)
+  }
+}.compact.sort.max
