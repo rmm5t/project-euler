be65dcb81e01cb1c295bc15eece34cc63cf02500
Daniel Ackerman <daniel.joseph.ackerman@gmail.com>
2014-04-27 18:57:31 -0400

    Added a check for negative numbers to all projects

--- 004/largest_palindrome_product.rb
+++ 004/largest_palindrome_product.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 number = (ARGV[0] || 1000).to_i
+abort("Cannot input a negative number") unless number > 0
 print "The largest palindrome created by the product of two positive numbers under #{number} is: "
 puts (1...number).flat_map { |n|
   (n...number).flat_map { |t|
--- 010/sum_primes.rb
+++ 010/sum_primes.rb
@@ -3,5 +3,6 @@
 
 require "prime"
 max = (ARGV[0] || 2_000_000).to_i
+abort("Cannot input a negative number") unless max > 0
 print "Sum of primes below #{max}: "
 puts Prime.each(max - 1).inject(&:+)
